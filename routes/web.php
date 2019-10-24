c3f55be5ecfb8032c24723c6e34f0d0bc2f1fca0
Iulian <iuliancmoldovanu@gmail.com>
2019-10-24 19:26:45 +0100

    Update: logging in a  single file

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
